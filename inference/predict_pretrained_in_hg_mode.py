b99ed4690188d4ba2515516076a0247bd5a87242
Xu,Anhua <xuanhuapku@163.com>
2024-08-30 13:03:26 +0800

    Update inference/predict_pretrained_in_hg_mode.py

--- inference/predict_pretrained_in_hg_mode.py
+++ inference/predict_pretrained_in_hg_mode.py
@@ -1,23 +1,38 @@
 
-from config import CHATGLM_6B_V2_BASE_MODEL_PATH
-
-
+import argparse
+import os
+from tqdm import tqdm
+import torch
+import json
 from transformers import AutoTokenizer, AutoModel
+
+from  utils import save_data_with_pprint
+
+from config import CHATGLM_6B_V2_BASE_MODEL_PATH
+from config import DATA_HOME
 
 tokenizer = AutoTokenizer.from_pretrained(CHATGLM_6B_V2_BASE_MODEL_PATH, trust_remote_code=True)
 model = AutoModel.from_pretrained(CHATGLM_6B_V2_BASE_MODEL_PATH, trust_remote_code=True).half().cuda()
 model = model.eval()
 
+# We might use it later for setting up different models, testing data paths, and other parameters
+#def set_args():
+#  parser = argparse.ArgumentParser()
+#  parser.add_argument('--test_path', default='data/tb_1.jsonl', type=str, help='')
+#  parser.add_argument('--device', default='1', type=str, help='')
+#  parser.add_argument('--max_len', type=int, default=768, help='Maximum allowed length of tokens, including the prompt part and generated part')
+#  parser.add_argument('--max_src_len', type=int, default=450, help='Maximum allowed length of token in prompt part')
+#  parser.add_argument('--result_path', type=str, required=True, help='The path to save the predicted results' )
+#  return parser.parse_args()
 
-text = """
-你现在是一个行程预定助理，你从用户和机器人的对话中，提取出航班信息（出发城市、到达城市、出发时间）、酒店预定信息（入住时间、离店时间、入住城市）、火车票信息（出发城市、目的城市、出发时间）、用车信息（用车城市、开始日期、结束日期、用车次数）；下面是用户和机器人的对话：
+result = []
+input_path = os.path.join(DATA_HOME, "tb_1.jsonl") 
+result_path = os.path.join(DATA_HOME, "output", "tb_1_result_chatglm2_hg_mode.txt") 
+with open(input_path, "r", encoding="utf-8") as fh:
+        for i, line in enumerate(tqdm(fh, desc="iter")):
+            with torch.no_grad():
+                sample = json.loads(line.strip())
+                response, history = model.chat(tokenizer, sample['text'], history=[])
+                result.append(    {"text": sample["text"], "ori_answer": sample["answer"], "gen_answer": response }) 
 
-user: 我当前所在的城市是北京，今天的日期是2024年3月25日
-
-根据这些对话，你将生成如下JSON结构的回复内容:"
-"""
-
-response, history = model.chat(tokenizer, text, history=[])
-print(response)
-#response, history = model.chat(tokenizer, "晚上睡不着应该怎么办", history=history)
-#print(response)
+save_data_with_pprint(result, result_path)

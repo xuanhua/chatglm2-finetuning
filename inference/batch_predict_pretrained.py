355e752135af0dc445e683a5d668870af4925e5e
Xu,Anhua <xuanhuapku@163.com>
2024-09-01 17:29:43 +0800

    Add first version of batch based prediction for pre-trained model

---
+++ inference/batch_predict_pretrained.py
@@ -0,0 +1,140 @@
+import torch
+import json
+
+USE_CHATGLM_6B_V1 = False
+if USE_CHATGLM_6B_V1:
+    from chatglm_6b.modeling_chatglm import ChatGLMForConditionalGeneration
+    from chatglm_6b.tokenization_chatglm import ChatGLMTokenizer
+else:
+    from chatglm2_6b.modeling_chatglm import ChatGLMForConditionalGeneration
+    from chatglm2_6b.tokenization_chatglm import ChatGLMTokenizer
+
+
+#from modeling_chatglm import ChatGLMForConditionalGeneration
+#from tokenization_chatglm import ChatGLMTokenizer
+#from peft import get_peft_model, LoraConfig, TaskType, PeftModel
+from tqdm import tqdm
+import time
+import os
+import argparse
+from pprint import pprint
+from logzero import logger
+
+from transformers import AutoTokenizer
+
+
+if USE_CHATGLM_6B_V1:
+    from config import CHATGLM_6B_V1_BASE_MODEL_PATH
+    base_model_path = CHATGLM_6B_V1_BASE_MODEL_PATH
+else:
+    from config import CHATGLM_6B_V2_BASE_MODEL_PATH
+    base_model_path = CHATGLM_6B_V2_BASE_MODEL_PATH 
+
+def set_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--test_path', default='data/tb_1.jsonl', type=str, help='')
+    parser.add_argument('--device', default='1', type=str, help='')
+    parser.add_argument('--model_dir',
+                        default=base_model_path, 
+                        type=str,
+                        help='')
+    parser.add_argument('--max_len', type=int, default=768, help='Maximum allowed length of tokens, including the prompt part and generated part')
+    parser.add_argument('--max_src_len', type=int, default=450, help='Maximum allowed length of token in prompt part')
+    parser.add_argument('--result_path', type=str, required=True, help='The path to save the predicted results' )
+    return parser.parse_args()
+
+def main():
+    args = set_args()
+    model = ChatGLMForConditionalGeneration.from_pretrained(args.model_dir)
+    #tokenizer = ChatGLMTokenizer.from_pretrained(base_model_path, padding_side='left')
+    tokenizer = AutoTokenizer.from_pretrained(base_model_path, trust_remote_code=True)
+    model.eval()
+    model.half().to("cuda:{}".format(args.device))
+
+    save_data = []
+    f1 = 0.0
+    max_tgt_len = args.max_len - args.max_src_len - 3
+    s_time = time.time()
+
+    text_buf = [] 
+    ans_buf = []
+    with open(args.test_path, "r", encoding="utf-8") as fh:
+        for i, line in enumerate(tqdm(fh, desc="iter")):
+            with torch.no_grad():
+                sample = json.loads(line.strip())
+
+                if USE_CHATGLM_6B_V1:
+                    pass
+                else:
+                    sample["text"] = "[Round {}]\n\n问：{}\n\n答：".format(1, sample["text"])
+
+                if i == 0:
+                    text_buf.append(sample["text"])
+                    ans_buf.append(sample["answer"])
+                    continue
+                elif i == 1:
+                    text_buf.append(sample["text"])
+                    ans_buf.append(sample["answer"])
+                else:
+                    break
+
+                inputs = tokenizer(text_buf,  padding=True,  return_tensors='pt').to(f"cuda:{args.device}")
+
+                #if len(src_tokens) >= args.max_src_len:
+                #    logger.warning(f"{i}th src text is too long, skipping this line.")
+
+                #if USE_CHATGLM_6B_V1:
+                #    tokens = src_tokens + ["[gMASK]", "<sop>"]
+                #else:
+                #    tokens = ["[gMASK]", "<sop>"] + src_tokens
+
+                #input_ids = tokenizer.convert_tokens_to_ids(tokens)
+
+                #input_ids = torch.tensor(input_ids).to("cuda:{}".format(args.device))
+                generation_kwargs = {
+                    "min_length": 5,
+                    "max_new_tokens": max_tgt_len,
+                    "top_p": 0.1,
+                    "temperature": 0.7, # it must be positive
+                    "do_sample": True,
+                    "num_return_sequences": 1,
+                }
+                response = model.generate(**inputs, **generation_kwargs)
+
+                #res = []
+                # here batch_size = 2
+                batch_size = 2
+                for i_b in range(batch_size):
+                    start_offset = inputs["input_ids"].shape[1]
+                    # TODO: change 'num_return_sequences' to fix this part later
+                    #for i_r in range(generation_kwargs["num_return_sequences"]):
+                    outputs = response.tolist()[i_b][start_offset:]
+                    r = tokenizer.decode(outputs).replace("<eop>", "")
+                    #res.append(r)
+
+                    #pre_res = [rr for rr in res[0].split("\n") if len(rr.split("_")) == 3]
+                    #real_res = sample["answer"].split("\n")
+                    #same_res = set(pre_res) & set(real_res)
+                    #if len(set(pre_res)) == 0:
+                    #    p = 0.0
+                    #else:
+                    #    p = len(same_res) / len(set(pre_res))
+                    #r = len(same_res) / len(set(real_res))
+                    #if (p + r) != 0.0:
+                    #    f = 2 * p * r / (p + r)
+                    #else:
+                    #    f = 0.0
+                    #f1 += f
+                    save_data.append(
+                        {"text": text_buf[i_b], "ori_answer": ans_buf[i_b], "gen_answer": r})
+
+    e_time = time.time()
+    print("总耗时：{}s".format(e_time - s_time))
+    print(f1 / 50)
+    saved_path = args.result_path
+    os.makedirs(os.path.dirname(saved_path), exist_ok=True)
+    with open(saved_path, "w", encoding="utf-8") as fin:
+        pprint(save_data, fin)
+
+if __name__ == '__main__':
+    main()
